87fa7498e1714a60c1589f004bea36a89010cd34
fengwutianya <949979029@qq.com>
2016-08-22 20:45:23 +0800

    0.2

--- src/TankClient.java
+++ src/TankClient.java
@@ -1,4 +1,6 @@
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 /**
  * Created by xuan on 2016/8/22.
@@ -7,6 +9,13 @@ public class TankClient extends Frame{
     public void launchFrame() {
         setLocation(200, 200);
         setSize(800, 600);
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+        setTitle("TankWar");
         setResizable(false);
         setVisible(true);
     }
