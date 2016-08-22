61fc73814c8b49c311b08313c3eec70869e71b70
fengwutianya <949979029@qq.com>
2016-08-22 21:17:25 +0800

    0.4 tank动起来

--- src/TankClient.java
+++ src/TankClient.java
@@ -3,9 +3,51 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 /**
+ * IDE: Ctrl+Alt+T包裹代码
+ * IDE: Ctrl+o重写父类代码
+ * Java: 线程调用paint(), paint()调用repaint(), 内部类最好
  * Created by xuan on 2016/8/22.
  */
 public class TankClient extends Frame{
+    int x = 50; int y = 50;
+
+//    private class PaintThread extends Thread {
+//        @Override
+//        public void run() {
+//            while (true) {
+//                repaint();
+//                try {
+//                    Thread.sleep(150);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }
+//    }
+    private class PaintThread implements Runnable {
+    @Override
+    public void run() {
+        while (true) {
+                repaint();
+                try {
+                    Thread.sleep(150);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+    }
+}
+
+    @Override
+    public void paint(Graphics g) {
+        Color c = g.getColor();
+        g.setColor(Color.RED);
+        g.fillOval(x, y, 30, 30);
+        g.setColor(c);
+        y += 5;
+        x += 5;
+    }
+
     public void launchFrame() {
         setLocation(200, 200);
         setSize(800, 600);
@@ -18,9 +60,11 @@ public class TankClient extends Frame{
         setTitle("TankWar");
         setResizable(false);
         setVisible(true);
+        new Thread(new PaintThread()).start();
     }
     public static void main(String[] args) {
         TankClient tc = new TankClient();
         tc.launchFrame();
+//        tc.new PaintThread().start();
     }
 }
