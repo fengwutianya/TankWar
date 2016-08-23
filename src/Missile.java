8997953eeef641bf1a5ce415226cf317e87e8fd0
fengwutianya <949979029@qq.com>
2016-08-23 12:44:13 +0800

    1.1 Missile flying with some bugs to fix

--- src/Missile.java
+++ src/Missile.java
@@ -7,6 +7,8 @@ public class Missile {
     private int x, y;
     public static final int XSPEED = 20;
     public static final int YSPEED = 20;
+    public static final int WIDTH = 10;
+    public static final int HEIGHT = 10;
     Tank.Direction dir;
 
     public Missile(int y, int x, Tank.Direction dir) {
@@ -18,7 +20,7 @@ public class Missile {
     public void draw(Graphics g) {
         Color c = g.getColor();
         g.setColor(Color.black);
-        g.fillOval(x, y, 10, 10);
+        g.fillOval(x, y, WIDTH, HEIGHT);
         g.setColor(c);
         move();
     }
