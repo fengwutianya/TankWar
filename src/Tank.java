04f4f9096153c750db4440620a4302b4b2312a96
fengwutianya <949979029@qq.com>
2016-08-23 09:45:48 +0800

    0.8

--- src/Tank.java
+++ src/Tank.java
@@ -6,11 +6,50 @@ import java.awt.event.KeyEvent;
  */
 public class Tank {
     private int x, y;
-    public static final int SPEED = 15;
+    private boolean bL = false, bR = false, bU = false, bD = false;
+    enum Direction {L, LU, U, RU, R, RD, D, LD, STOP}
+    private Direction dir = Direction.STOP;
+    public static final int XSPEED = 15;
+    public static final int YSPEED = 15;
 
     public Tank(int x, int y) {
         this.x = x;
         this.y = y;
+    }
+
+    void move() {
+        switch (dir) {
+            case L:
+                x -= XSPEED;
+                break;
+            case LU:
+                x -= XSPEED;
+                y -= YSPEED;
+                break;
+            case U:
+                y -= YSPEED;
+                break;
+            case RU:
+                x += XSPEED;
+                y -= YSPEED;
+                break;
+            case R:
+                x += XSPEED;
+                break;
+            case RD:
+                x += XSPEED;
+                y += YSPEED;
+                break;
+            case D:
+                y += YSPEED;
+                break;
+            case LD:
+                x -= XSPEED;
+                y += YSPEED;
+                break;
+            case STOP:
+                break;
+        }
     }
 
     public void draw(Graphics g) {
@@ -18,13 +57,38 @@ public class Tank {
         g.setColor(Color.RED);
         g.fillOval(x, y, 30, 30);
         g.setColor(c);
+
+        move();
     }
 
     public void keyPressed(KeyEvent e) {
         int key = e.getKeyCode();
-        if (key == KeyEvent.VK_RIGHT) x += SPEED;
-        else if (key == KeyEvent.VK_LEFT) x -= SPEED;
-        else if (key == KeyEvent.VK_UP) y -= SPEED;
-        else if (key == KeyEvent.VK_DOWN) y += SPEED;
+        switch (key) {
+            case KeyEvent.VK_LEFT:
+                bL = true;
+                break;
+            case KeyEvent.VK_RIGHT:
+                bR = true;
+                break;
+            case KeyEvent.VK_UP:
+                bU = true;
+                break;
+            case KeyEvent.VK_DOWN:
+                bD = true;
+                break;
+        }
+        locationDirection();
+    }
+
+    void locationDirection() {
+        if (bL && !bU && !bR && !bD)            dir = Direction.L;
+        else if (bL && bU && !bR && !bD)        dir = Direction.LU;
+        else if (!bL && bU && !bR && !bD)       dir = Direction.U;
+        else if (!bL && bU && bR && !bD)        dir = Direction.RU;
+        else if (!bL && !bU && bR && !bD)       dir = Direction.R;
+        else if (!bL && !bU && bR && bD)        dir = Direction.RD;
+        else if (!bL && !bU && !bR && bD)       dir = Direction.D;
+        else if (bL && !bU && !bR && bD)        dir = Direction.LD;
+        else                                    dir = Direction.STOP;
     }
 }
